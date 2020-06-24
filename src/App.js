4d90fdc84b6a52c133bb6991e7575333ea31246c
Andrew Connors <connorsandrew1@gmail.com>
2020-06-24 10:22:12 -0300

    Added Twitch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import YouTube from 'react-youtube';
+import ReactTwitchEmbedVideo from "react-twitch-embed-video"
 import './App.css';
 
 class App extends Component{
@@ -16,17 +17,23 @@ class App extends Component{
       <div className="App">
         <h1 className="header">Andrew & Jacqueline <br /> Live Wedding Stream</h1>
         <h5 className="centered subheading">
-          The ceremony is scheduled to begin on July 4th, 2020 at 4pm AST.
+          The ceremony is scheduled to begin on July 4th, 2020 at 4:30pm AST.
           Tune in then to watch!
         </h5>
-        {process.env.REACT_APP_VIDEO_ID &&
+        {process.env.REACT_APP_YOUTUBE_VIDEO_ID &&
           <div>
-            <YouTube videoId={process.env.REACT_APP_VIDEO_ID} opts={options}/>;
+            <YouTube videoId={process.env.REACT_APP_VIDEO_ID} opts={options}/>
           </div>
         }
-        {!process.env.REACT_APP_VIDEO_ID &&
+        {process.env.REACT_APP_TWITCH_CHANNEL &&
           <div>
-            <img className="placeholder-image" src="https://cdn0.weddingwire.ca/usr/5/6/4/7/sfxb_663284.jpg"></img>
+            <ReactTwitchEmbedVideo channel={process.env.REACT_APP_TWITCH_CHANNEL} />
+
+          </div>
+        }
+        {!process.env.REACT_APP_TWITCH_CHANNEL && !process.env.REACT_APP_YOUTUBE_VIDEO_ID &&
+          <div>
+            <img alt={"Andrew&Jacqueline"} className="placeholder-image" src="https://cdn0.weddingwire.ca/usr/5/6/4/7/sfxb_663284.jpg"></img>
           </div>
         }
       </div>
